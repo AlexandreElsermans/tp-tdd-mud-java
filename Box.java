2eccf452c43a4f3833134c4a711b82dbf896c5bf
ELSERMANS Alexandre <alex.elsermans@gmail.com>
2025-12-02 16:13:21 +0100

    premiers test java

---
+++ Box.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Box {
+    ArrayList<String> contents = new ArrayList<String>();
+    
+    Box() {System.out.println("Box créée");}
+
+    public void add(String truc) {
+        this.contents.add(truc);
+    }
+}
