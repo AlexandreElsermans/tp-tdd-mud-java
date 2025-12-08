aa68f105a9fffbbed945f7c7aaf68d79896dd638
ELSERMANS Alexandre <alex.elsermans@gmail.com>
2025-12-08 11:00:56 +0100

    ajout test remove

--- Box.java
+++ Box.java
@@ -1,11 +1,25 @@
 import java.util.ArrayList;
 
 public class Box {
-    ArrayList<Thing> contents = new ArrayList<Thing>();
+    private ArrayList<Thing> contents = new ArrayList<Thing>();
     
     Box() {System.out.println("Box créée");}
 
     public void add(Thing truc) {
         this.contents.add(truc);
+    }
+
+    public ArrayList<Thing> getContents(){
+        return this.contents;
+    }
+
+    public boolean contains(Thing objet) {
+        return this.contents.contains(objet);
+    }
+
+    public void remove(Thing objet) throws  RuntimeException{
+        if (!this.contents.remove(objet)) {
+            throw new RuntimeException("Remove impossible : l'objet n'est pas dans la Box");
+        }
     }
 }
--- TestBoxes.java
+++ TestBoxes.java
@@ -1,5 +1,6 @@
-import org.junit.*;
+import  org.junit.Test;
 import static org.junit.Assert.*;
+
 
 public class TestBoxes {
     @Test 
@@ -14,5 +15,44 @@ public class TestBoxes {
         Thing truc2 = new Thing("truc2");
         b.add(truc1);
         b.add(truc2);
+    }
+
+    @Test
+    public void testGetContents() {
+        Box b = new Box();
+        Thing truc1 = new Thing("truc1");
+        b.add(truc1);
+        b.getContents();
+    }
+
+    @Test
+    public void testContentsNull() {
+        Box b = new Box();
+        Thing truc1 = new Thing("truc1");
+        Thing truc2 = new Thing("truc1");
+        b.add(truc1);
+        assertTrue(b.contains(truc1));
+        assertFalse(b.contains(truc2));
+    }
+
+    @Test
+    public void testBoxRemove(){
+        Box b = new Box();
+        Thing truc1 = new Thing("truc1");
+        Thing truc2 = new Thing("truc2");
+        b.add(truc1);
+        b.add(truc2);
+        b.remove(truc2);
+        assertTrue(b.contains(truc1));
+        assertFalse(b.contains(truc2));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testBoxRemoveException() {
+        Box b = new Box();
+        Thing truc1 = new Thing("truc1");
+        Thing truc2 = new Thing("truc2");
+        b.add(truc1);
+        b.remove(truc2);
     }
 }
